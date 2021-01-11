47bb374c49aff5bee4bd23c4b6a1f4ddb8f2bb15
ablaev.dmitry <dablaev@yandex.ru>
2021-01-11 14:21:37 +0700

    Update
    -Added main menu scene,
    -Baked lightmap on 2 tracks,
    -Old car shaders replaced on new.

---
+++ Assets/PagesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PagesController : MonoBehaviour
+{
+    public GameObject currentPage;
+    public GameObject[] pages;
+    
+    public void OpenPage(int pageInd)
+    {
+        currentPage.SetActive(false);
+        currentPage = pages[pageInd];
+        currentPage.SetActive(true);
+    }
+}
