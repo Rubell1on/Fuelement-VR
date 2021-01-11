69cf8aab4749312acf30de6449a68898770d9da4
ablaev.dmitry <dablaev@yandex.ru>
2021-01-11 16:37:41 +0700

    Added launch Fuelement transition

---
+++ Assets/FuelementVidController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelementVidController : MonoBehaviour
+{
+    public CanvasGroup wrapper;
+    public RectTransform logo;
+    public enum FulementVidEventType { onFinish };
+
+    public void Launch()
+    {
+        LTSeq seq = LeanTween.sequence();
+        seq.append(FadeOut(0f))
+            .append(FadeIn(3f))
+            .append(LeanTween.size(logo, new Vector2(270, 270), 5f))
+            .append(FadeOut(0.5f))
+            .append(() => LeanTween.dispatchEvent((int)FulementVidEventType.onFinish));
+
+    }
+
+    public LTDescr FadeIn(float duration)
+    {
+        return LeanTween.alphaCanvas(wrapper, 1, duration);
+    }
+
+    public LTDescr FadeOut(float duration)
+    {
+        return LeanTween.alphaCanvas(wrapper, 0, duration).setEase(LeanTweenType.easeOutExpo);
+    }
+}
---
+++ Assets/LaunchVidsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchVidsController : MonoBehaviour
+{
+    public GameObject fuelementLogo;
+    public GameObject mainMenu;
+    public bool noVids = false;
+
+    void Start()
+    {
+        if (!noVids)
+        {
+            LeanTween.addListener((int)FuelementVidController.FulementVidEventType.onFinish, e =>
+            {
+                fuelementLogo.SetActive(false);
+                mainMenu.SetActive(true);
+            });
+
+            FuelementVidController fVC;
+
+            if (fuelementLogo.TryGetComponent(out fVC))
+            {
+                fVC.Launch();
+            }
+        } else
+        {
+            fuelementLogo.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+}
